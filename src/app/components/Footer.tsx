31fe23e9f03e877167b1762b9ca2a178bc01011f
JuulWilhelm <julia@levarne.nl>
2023-12-19 00:43:58 +0100

    Added MediaElements, Updated

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,19 @@
+import MediaElements from "./MediaElements";
+
 const Footer: React.FC = () => {
   return (
     <footer className="wrapper--footer">
       <p>©2023, All right reserved.</p>
-      <p>Footer menu elements</p>
-      <p>Icons</p>
+      <div className="footer--menu">
+        <ul>
+          <li>Privacy Policy</li>
+          <li>Terms of Service</li>
+          <li>Cookies Settings</li>
+        </ul>
+      </div>
+      <div className="footer--media">
+        <MediaElements />
+      </div>
     </footer>
   );
 };
