b3b14fe9f3a090bc594bc310a55762727d84f959
JuulWilhelm <julia@levarne.nl>
2023-12-19 00:15:27 +0100

    Added and styled image, added text content to right part of contact box

--- src/app/components/ContactBox.tsx
+++ src/app/components/ContactBox.tsx
@@ -1,7 +1,18 @@
 const ContactBox: React.FC = () => {
   return (
-    <div>
-      <h1>Contactbox</h1>
+    <div className="wrapper--contact-box contact-box">
+      <div className="contact-box--element">
+        <div className="contact-box--img">
+          <img src="./doggo.jpg" alt="doggo" />
+          <div className="overlay"></div>
+        </div>
+      </div>
+      <div className="contact-box--element">
+        <h3>Wil je ook een passende strategie voor jou digitale missie?</h3>
+        <p>Bel of mail Laurens, creatief directeur.</p>
+        <p>030 27 69 167 / 06 412 75 222 </p>
+        <p>laurens@schaapontwerpers.nl</p>
+      </div>
     </div>
   );
 };
